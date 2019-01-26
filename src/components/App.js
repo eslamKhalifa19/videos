9e52f8445d201ab5c880ef0ee44a074ab8a66459
eslam <eslamkhalefa19@gmail.com>
2019-01-26 16:24:11 +0200

    Search bar

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
